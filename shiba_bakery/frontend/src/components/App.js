471556e7dab67ed84c014b5f31aa49729b671bb5
suru <dragos.surugiu13@yahoo.com>
2021-10-12 15:39:54 +0300

    Some stuff here

--- shiba_bakery/frontend/src/components/App.js
+++ shiba_bakery/frontend/src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Signup from "./signup";
 
-function App(){
-    return(
-        <div className="App">
-            <Signup/>
-        </div>
-    )
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return(
+            <div className="App">
+                <Signup/>
+            </div>
+        );
+    }
 
 }
 
-export default App;
+// return(
+//     <div className="App">
+//         <Signup/>
+//     </div>
+// )
+
+//export default App;
